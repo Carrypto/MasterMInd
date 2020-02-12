e53dbd62d0a9e8236cdd31c1e7d09343a14deb95
Carrypto <mzagrodzki8@gmail.com>
2020-02-12 16:00:45 +0100

    preferences

--- app/src/main/java/pl/zhr/hak/mastermind/SettingsActivity.java
+++ app/src/main/java/pl/zhr/hak/mastermind/SettingsActivity.java
@@ -4,46 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button startButton;
-    Button scoreButton;
-    Button settingsButton;
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startButton.findViewById(R.id.startButton);
-        scoreButton.findViewById(R.id.scoreButton);
-        settingsButton.findViewById(R.id.settingsButton);
-
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        scoreButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-
-
+        setContentView(R.layout.activity_settings);
     }
 
     @Override
